10d3df3d4c9be2e565cf5f19ffec9167d327c74d
RogerSelwyn <rogers@selwyn.org.uk>
2025-09-10 08:42:15 +0100

    fix: Fix error in migration from O365

--- custom_components/ms365_todo/integration/filemgmt_integration.py
+++ custom_components/ms365_todo/integration/filemgmt_integration.py
@@ -1,6 +1,7 @@
 """To-Do file management processes."""
 
 import logging
+import os
 
 import yaml
 from homeassistant.const import CONF_NAME
@@ -44,6 +45,9 @@ def load_yaml_file(path, item_id, item_schema):
 
 def write_yaml_file(yaml_filepath, task):
     """Write the tasks file entry"""
+    dirpath = os.path.dirname(yaml_filepath)
+    if not os.path.isdir(dirpath):
+        os.makedirs(dirpath)
     with open(yaml_filepath, "a", encoding="UTF8") as out:
         out.write("\n")
         yaml.dump([task], out, default_flow_style=False, encoding="UTF8")
